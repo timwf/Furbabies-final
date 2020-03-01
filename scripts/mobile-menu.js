4bf16a951385b12ba22b00433193856ebc9e8c92
Timothy Williams-Fowler <timwfowler0@gmail.com>
2020-03-01 12:55:52 +0700

    Add files via upload

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -5,19 +5,23 @@ var mobBtn = document.getElementById("mob-btn");
 var mobCont = document.getElementById("mob-menu-cont");
 var mobOver = document.getElementById("mob-over");
 var mobButton = document.getElementById("hamb");
+var contactUs = document.getElementById("contact-us-mob");
 
 
-mobButton.addEventListener("click", function(){
+contactUs.addEventListener("click", function(){
   mobButton.classList.toggle("animate");
+  mobCont.classList.remove("mob-menu-active");
+  mobOver.classList.remove("mob-over-active");
 
+  window.scrollTo(0,document.body.scrollHeight);
+    });
 
-
-
+mobBtn.addEventListener("click", function(){
+  mobButton.classList.toggle("animate");
     });
 
 
 
-console.log(mobBtn);
 
 mobBtn.addEventListener("click", function(){
   mobCont.classList.toggle("mob-menu-active");
